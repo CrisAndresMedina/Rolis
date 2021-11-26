30d5c5cd4aaef5cfb279798be59f8571d7d5abac
CrisAndresMedina <cryl154@misena.edu.co>
2021-11-25 23:21:09 -0500

    lo de hoy

--- app/src/main/java/com/example/rolis/MainActivity.java
+++ app/src/main/java/com/example/rolis/MainActivity.java
@@ -3,11 +3,12 @@ package com.example.rolis;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity : AppCompatActivity()    {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+setSupportActionBar(findViewById(R.id.my_toolbar))
     }
 }
